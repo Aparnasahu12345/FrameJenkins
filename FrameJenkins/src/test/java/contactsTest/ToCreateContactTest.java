43cc6db8f632e8c9db31a199e2a3d8e8e56347f2
arunk <arunk@Arun>
2024-11-13 19:35:25 +0530

    Pushed

---
+++ FrameJenkins/src/test/java/contactsTest/ToCreateContactTest.java
@@ -0,0 +1,12 @@
+package contactsTest;
+
+public class ToCreateContactTest {
+
+	public static void main(String[] args) {
+	
+		
+		System.out.println("Successgully created contact test");
+
+	}
+
+}
---
+++ FrameJenkins/src/test/java/organisationsTest/ToCreateOrganisationTest.java
@@ -0,0 +1,10 @@
+package organisationsTest;
+
+public class ToCreateOrganisationTest {
+
+	public static void main(String[] args) {
+		  System.out.println("Successfully created organisation test");
+
+	}
+
+}
